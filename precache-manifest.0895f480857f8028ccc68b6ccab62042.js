3b1894553c71c3f7fc43234e1daeb9be4b2a1400
pythonguru101 <pythonguru101@gmail.com>
2023-02-22 13:03:28 -0800

    Updates

---
+++ precache-manifest.0895f480857f8028ccc68b6ccab62042.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbfbeb001818795cb647b36c0aa91eb",
+    "url": "/Personal-Portfolio-Daniel/index.html"
+  },
+  {
+    "revision": "97f564af16f8e57b7f1c",
+    "url": "/Personal-Portfolio-Daniel/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "f38589d4a41b3b923068",
+    "url": "/Personal-Portfolio-Daniel/static/css/main.b7965308.chunk.css"
+  },
+  {
+    "revision": "97f564af16f8e57b7f1c",
+    "url": "/Personal-Portfolio-Daniel/static/js/2.6a04a4f4.chunk.js"
+  },
+  {
+    "revision": "0db18f2f56e579bdbf0e639ec3e0b99a",
+    "url": "/Personal-Portfolio-Daniel/static/js/2.6a04a4f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38589d4a41b3b923068",
+    "url": "/Personal-Portfolio-Daniel/static/js/main.e8dd5a68.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Personal-Portfolio-Daniel/static/js/main.e8dd5a68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f56fff3d7d2880e96d",
+    "url": "/Personal-Portfolio-Daniel/static/js/runtime-main.1b3017d2.js"
+  },
+  {
+    "revision": "510540a77919dad4220302e8400b7c8c",
+    "url": "/Personal-Portfolio-Daniel/static/media/DanielP.510540a7.png"
+  },
+  {
+    "revision": "1c6f0557fce7b7fd784489885c98cb7e",
+    "url": "/Personal-Portfolio-Daniel/static/media/OOO-Demand.1c6f0557.png"
+  },
+  {
+    "revision": "f56961d42d079d7ba8416ecfd484ee6a",
+    "url": "/Personal-Portfolio-Daniel/static/media/aiswitch.f56961d4.png"
+  },
+  {
+    "revision": "5dd99c60795ec0cbc860c2b9e98e0d9b",
+    "url": "/Personal-Portfolio-Daniel/static/media/basecampclimbing.5dd99c60.png"
+  },
+  {
+    "revision": "6326b03b6545cc6d266b6a7be372d732",
+    "url": "/Personal-Portfolio-Daniel/static/media/blogg.6326b03b.png"
+  },
+  {
+    "revision": "8177653835fc7de5116aabde9bc8a3ce",
+    "url": "/Personal-Portfolio-Daniel/static/media/boarddirector.81776538.png"
+  },
+  {
+    "revision": "e0607cf770abc9388582687dfa0c1094",
+    "url": "/Personal-Portfolio-Daniel/static/media/dafisha.e0607cf7.png"
+  },
+  {
+    "revision": "004e08e37417768579a19aae4355700c",
+    "url": "/Personal-Portfolio-Daniel/static/media/livesmart.004e08e3.png"
+  },
+  {
+    "revision": "bad637333753ffa4f7ed34c9c0c63d87",
+    "url": "/Personal-Portfolio-Daniel/static/media/ln2x.bad63733.png"
+  },
+  {
+    "revision": "f44350d89e07302a5284aa994c5f72b0",
+    "url": "/Personal-Portfolio-Daniel/static/media/r1.f44350d8.png"
+  },
+  {
+    "revision": "84c27bb5a840fd42fc2ba10625d2b83c",
+    "url": "/Personal-Portfolio-Daniel/static/media/reservation.84c27bb5.png"
+  },
+  {
+    "revision": "2c249a2b6874a4d717e35a00bbb432e0",
+    "url": "/Personal-Portfolio-Daniel/static/media/rideto.2c249a2b.png"
+  },
+  {
+    "revision": "24a59637833b289bfbaab35f5f08f71a",
+    "url": "/Personal-Portfolio-Daniel/static/media/savingschampion.24a59637.png"
+  },
+  {
+    "revision": "e324d3000236bc43b95fd0d1d88ec37f",
+    "url": "/Personal-Portfolio-Daniel/static/media/shake.e324d300.svg"
+  }
+]);
